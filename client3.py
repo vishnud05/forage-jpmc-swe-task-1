76a44a64129c6bd14069cfe7a5774e736446f179
Vishnu Dev Aggarwal <vishnu.3b@gmail.com>
2024-07-11 13:15:20 +0530

    add: tests for getRatio and getDataPoint

--- client3.py
+++ client3.py
@@ -42,6 +42,9 @@ def getDataPoint(quote):
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
+    if(price_a < 0 or price_b < 0):
+        return 
+
     if(price_b == 0):
         return
     
